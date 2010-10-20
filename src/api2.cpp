7a112ac8e973833409a9f9aec0cbd5f82440b716
Ilya Dogolazky <ilya.dogolazky@nokia.com>
2010-10-20 23:33:33 +0300

    continued api rework (api2)

--- src/api2.cpp
+++ src/api2.cpp
@@ -1,10 +1,15 @@
+#include <cstdio>
+#include <cstring>
+
+#include <string>
+#include <set>
+using namespace std ;
+
 #include "api2.h"
 
 namespace qmlog
 {
-  dispatcher_t *object_t::current_dispatcher = NULL ;
-  dispatcher_t *object_t::master = NULL ;
-  set<slave_dispatcher_t *> object_t::slaves ;
+  object_t object ;
 
   object_t::object_t()
   {
@@ -20,13 +25,302 @@ namespace qmlog
     slave_dispatcher_t *d = new slave_dispatcher_t ;
     // TODO: set default options and loggers...
     register_slave(d) ;
-    object_t::current_dispatcher = d ;
+    current_dispatcher = d ;
+    master = NULL ;
+    name = get_process_name() ;
   }
 
-  void object::register_slave(slave_dispatcher_t *d)
+  string object_t::get_process_name()
+  {
+    static const char *anonymous = "<unknown>" ;
+    const unsigned int len = 256 ;
+    char buf[len+1] ;
+
+    snprintf(buf, len, "/proc/%d/cmdline", getpid()) ;
+
+    // just being a bit paranoid (a decimal number will be never so long):
+    buf[len] = '\0' ;
+
+    FILE *fp = fopen(buf, "r") ;
+
+    if(fp==NULL)
+      return anonymous ;
+
+    string name ;
+
+    for(bool word_done=false; not word_done; )
+    {
+      int x = fread(buf, 1, len, fp) ;
+      buf[x] = '\0' ;
+      word_done = strlen(buf) < len ;
+      name += buf ;
+    }
+
+    const char *p = name.c_str(), *q = strrchr(p, '/') ;
+
+    if(q==NULL) // no slash at all
+      return name ;
+
+    if(q[1]=='\0') // trailing slash, again paranoid
+      return anonymous ;
+
+    return q+1 ;
+  }
+
+  void object_t::init()
+  {
+    static bool first = true ;
+    if (not first)
+    {
+      return ; // XXX: log it
+    }
+    first = false ;
+
+    current_dispatcher = master = new dispatcher_t ;
+  }
+
+  void object_t::register_slave(slave_dispatcher_t *d)
   {
     slaves.insert(d) ;
   }
+
+  dispatcher_t::dispatcher_t(const char *name)
+  {
+    this->name = (name==NULL) ? qmlog::object.get_name() : string(name) ;
+    last_pid = (pid_t) 0 ;
+    current_level = qmlog::Full ;
+  }
+
+  int dispatcher_t::log_level(int new_level)
+  {
+    return current_level = new_level ;
+  }
+
+  int dispatcher_t::log_level()
+  {
+    return current_level ;
+  }
+
+  void dispatcher_t::attach(abstract_log_t *l)
+  {
+    logs.insert(l) ;
+  }
+
+  void dispatcher_t::detach(abstract_log_t *l)
+  {
+    logs.erase(l) ;
+  }
+
+  void dispatcher_t::message(int level)
+  {
+    if (level<=current_level)
+      message(level, -1, NULL, NULL, "") ;
+  }
+
+  void dispatcher_t::message(int level, const char *fmt, ...)
+  {
+    if (level<=current_level)
+    {
+      va_list arg ;
+      va_start(arg, fmt) ;
+      generic(level, -1, NULL, NULL, fmt, arg) ;
+      va_end(arg) ;
+    }
+  }
+
+  void dispatcher_t::message(int level, int line, const char *file, const char *func)
+  {
+    if (level<=current_level)
+      message(level, line, file, func, "") ;
+  }
+
+  void dispatcher_t::message(int level, int line, const char *file, const char *func, const char *fmt, ...)
+  {
+    if (level<=current_level)
+    {
+      va_list arg ;
+      va_start(arg, fmt) ;
+      generic(level, line, file, func, fmt, arg) ;
+      va_end(arg) ;
+    }
+  }
+
+  void dispatcher_t::generic(int level, int line, const char *file, const char *func, const char *fmt, va_list arg)
+  {
+    for(set<abstract_log_t*>::iterator it=logs.begin(); it!=logs.end(); ++it)
+      if (level<=(*it)->log_level())
+        (*it)->compose_message(level, line, file, func, fmt, arg) ;
+  }
+
+  abstract_log_t::abstract_log_t(int maximal_log_level, dispatcher_t *d)
+  {
+    level = max_level = maximal_log_level ;
+    dispatcher = d ;
+    dispatcher->attach(this) ;
+  }
+
+  int abstract_log_t::log_level(int new_level)
+  {
+    if(new_level<=max_level)
+      level = new_level ;
+    return level ;
+  }
+
+  int abstract_log_t::log_level()
+  {
+    return level ;
+  }
+
+  int abstract_log_t::set_fields(int mask)
+  {
+    return fields = mask ;
+  }
+
+  int abstract_log_t::get_fields()
+  {
+    return fields ;
+  }
+
+  int abstract_log_t::enable_fields(int mask)
+  {
+    return fields |= mask ;
+  }
+
+  int abstract_log_t::disable_fields(int mask)
+  {
+    return fields &= ~mask ;
+  }
+
+  abstract_log_t::~abstract_log_t()
+  {
+    dispatcher->detach(this) ;
+  }
+
+  void abstract_log_t::compose_message(int level, int line, const char *file, const char *func, const char *fmt, va_list args)
+  {
+    smart_buffer<1024> buf ;
+    const char *separator = "" ;
+    if (fields & Time_Info_Block)
+    {
+      const char *ti_separator = "" ;
+      buf.printf("[") ;
+      if (int mono = fields & qmlog::Monotonic_Mask)
+      {
+        dispatcher->get_timestamp() ;
+        buf.printf("%s", dispatcher->str_monotonic()) ;
+        if (mono & qmlog::Monotonic_Milli)
+        {
+          if (mono & qmlog::Monotonic_Nano)
+            buf.printf(".%s", dispatcher->str_monotonic_nano()) ;
+          else if (mono & qmlog::Monotonic_Micro)
+            buf.printf(".%s", dispatcher->str_monotonic_micro()) ;
+          else
+            buf.printf(".%s", dispatcher->str_monotonic_milli()) ;
+        }
+        ti_separator = " " ;
+      }
+      if (int tz = fields & qmlog::Timezone_Tm_Block)
+      {
+        buf.printf("%s(", ti_separator) ;
+        dispatcher->get_struct_tm() ;
+        const char *tz_separator = "" ;
+        if (tz & qmlog::Timezone_Offset)
+        {
+          buf.printf("%s", dispatcher->str_gmt_offset()) ;
+          tz_separator = "," ;
+        }
+        if (tz & qmlog::Timezone_Abbreviation)
+          buf.printf("%s%s", tz_separator, dispatcher->str_tz_abbreviation()) ;
+        buf.printf(")") ;
+        ti_separator = " " ;
+      }
+      if (fields & qmlog::Date)
+      {
+        buf.printf("%s%s", ti_separator, dispatcher->str_date()) ;
+        ti_separator = " " ;
+      }
+      if (int time = fields & qmlog::Time_Mask)
+      {
+        buf.printf("%s%s", ti_separator, dispatcher->str_time()) ;
+        if (time & qmlog::Time_Milli)
+        {
+          if (time & qmlog::Time_Micro)
+            buf.printf(".%s", dispatcher->str_time_micro()) ;
+          else
+            buf.printf(".%s", dispatcher->str_time_milli()) ;
+        }
+        ti_separator = " " ;
+      }
+      if (fields & qmlog::Timezone_Symlink)
+        buf.printf("%s%s", ti_separator, dispatcher->str_tz_symlink()) ;
+      buf.printf("]") ;
+      separator = " " ;
+    }
+    if (fields & Process_Block) // [program:123] | [program] | [123]
+    {
+      const char *p_separator = "" ;
+      buf.printf("%s[", separator) ;
+      if (fields & Name)
+      {
+        buf.printf("%s", dispatcher->str_name()) ;
+        p_separator = ":" ;
+      }
+      if (fields & Pid)
+        buf.printf("%s%s", p_separator, dispatcher->str_pid()) ;
+      buf.printf("]") ;
+      separator = " " ;
+    }
+    buf.printf("%s", separator) ;
+    int prefix = buf.position() ;
+
+    bool message = (*fmt!='\0') && (fields & qmlog::Message) ;
+    bool output_line = line>0 && (fields & qmlog::Line) ;
+    bool output_func = func!=NULL && (fields & qmlog::Function) ;
+    bool location = output_line or output_func ;
+    bool wrap = output_func and (fields & qmlog::Multiline) ;
+
+    if (fields & qmlog::Level)
+    {
+      buf.printf("%s%s", separator, dispatcher->str_level()) ;
+      if (not message and not location)
+        buf.printf(".") ;
+      else if (not message or not location)
+        buf.printf(":") ;
+      separator = " " ;
+    }
+
+    if (output_line)
+    {
+      buf.printf("%s" "at %s:%d", separator, file, line) ;
+      separator = " " ;
+    }
+
+    if (output_func)
+    {
+      buf.printf("%s" "in %s", separator, func) ;
+      separator = " " ;
+    }
+
+    if (message and location)
+      buf.printf(":") ;
+
+    if (wrap)
+    {
+      submit_message(level, buf.c_str()) ;
+      buf.rewind(prefix) ;
+      separator = "-> " ;
+    }
+
+    if (message)
+    {
+      buf.printf("%s", separator) ;
+      buf.vprintf(fmt, args) ;
+    }
+
+    submit_message(level, buf.c_str()) ;
+  }
+
+
 
 }
 
--- src/api2.h
+++ src/api2.h
@@ -1,54 +1,238 @@
 #ifndef QMLOG_DISPATHCER_H
 #define QMLOG_DISPATHCER_H
 
-#include "qm..."
+// :xa
+// #include "qm..."
+//
+#include <string>
+#include <set>
+
+#include <stdarg.h>
 
 #ifndef QMLOG_DEFAULT_LOGGING_DISPATCHER
 #define QMLOG_DEFAULT_LOGGING_DISPATCHER (qmlog::object.get_current_dispatcher())
 #endif
 
+template<int bytes>
+struct smart_buffer
+{
+  char a[bytes], *p ;
+  unsigned len, pos ;
+
+  smart_buffer()
+  {
+    p = a ;
+    len = bytes ;
+    pos = 0 ;
+  }
+
+  unsigned position()
+  {
+    return pos ;
+  }
+
+  void rewind(unsigned at)
+  {
+    if (at<=pos)
+      pos = at ;
+  }
+
+  const char *c_str()
+  {
+    return p ;
+  }
+
+  void grow()
+  {
+    unsigned new_len = len + len ;
+    char *q = new char[new_len] ;
+    memcpy(q, p, pos) ;
+    if (p!=a)
+      delete[] p ;
+    p = q ;
+  }
+
+ ~smart_buffer()
+  {
+    if (p!=a)
+      delete[] p ;
+  }
+
+  void printf(const char *fmt, ...) ;
+  void vprintf(const char *fmt, va_list args) ;
+} ;
+
 namespace qmlog
 {
+  enum fields
+  {
+    Multiline             = 1 << 0,
+    Message               = 1 << 1,
+    Line                  = 1 << 2,
+    Function              = 1 << 3,
+    Pid                   = 1 << 4,
+    Name                  = 1 << 5,
+    Monotonic             = 1 << 6,
+    Monotonic2            = 1 << 7,
+    Monotonic3            = 1 << 8,
+    Monotonic4            = 1 << 9,
+    Monotonic_Milli       = Monotonic|Monotonic2,
+    Monotonic_Micro       = Monotonic|Monotonic2|Monotonic3,
+    Monotonic_Nano        = Monotonic|Monotonic2|Monotonic3|Monotonic4,
+    Date                  = 1 << 10,
+    Time                  = 1 << 11,
+    Time2                 = 1 << 12,
+    Time3                 = 1 << 13,
+    Time4                 = 1 << 14, // unused
+    Time_Milli            = Time|Time2,
+    Time_Micro            = Time|Time2|Time3,
+    Timezone_Symlink      = 1 << 15,
+    Timezone_Abbreviation = 1 << 16,
+    Timezone_Offset       = 1 << 17,
+    Level                 = 1 << 18,
+
+    Monotonic_Mask        = Monotonic|Monotonic2|Monotonic3|Monotonic4,
+    Time_Mask             = Time|Time2|Time3|Time4,
+    Timestamp_Mask        = Monotonic_Mask|Time_Mask|Date|Timezone_Abbreviation|Timezone_Offset,
+    Time_Info_Block       = Timestamp_Mask|Timezone_Symlink,
+    Timezone_Tm_Block     = Timezone_Offset|Timezone_Abbreviation,
+    Process_Block         = Name|Pid,
+    Location_Block        = Line|Function,
+  } ;
+
+  enum levels
+  {
+    Internal = 0,
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Notice = 4,
+    Info = 5,
+    Debug = 6,
+
+    Full = Debug
+  } ;
+
   class object_t ;
-  class abstract_log_t ;
   class dispatcher_t ;
   class slave_dispatcher_t ;
+  class abstract_log_t ;
+  class log_file ;
+  class log_stderr ;
+  class log_stdout ;
+  class log_syslog ;
+  class settings_modifier ;
+
+  extern object_t object ;
 
   class object_t
   {
     static object_t object ;
-    static dispatcher_t *current_dispatcher ;
-    static dispatcher_t *master ;
-    static set<slave_dispatcher_t *> slaves ;
+    static std::string get_process_name() ;
+    std::string name ;
+    dispatcher_t *current_dispatcher ;
+    dispatcher_t *master ;
+    std::set<slave_dispatcher_t *> slaves ;
   public:
+    void init() ;
+    string get_name() ;
     object_t() ;
     void register_slave(slave_dispatcher_t *) ;
-    static inline dispatcher_t *get_current_dispatcher() { return current_dispatcher ; }
-  } ;
-
-  class abstract_log_t
-  {
+    static inline dispatcher_t *get_current_dispatcher() { return object.current_dispatcher ; }
   } ;
 
   class dispatcher_t
   {
-
-    string name ;
-    set<abstract_log_t *> logs ;
+    std::string name ;
+    std::set<abstract_log_t *> logs ;
+    int current_level ;
     pid_t last_pid ;
     std::string pid_s ;
   public:
-    void attach(abstract_log_t) ;
+    dispatcher_t(const char *name=NULL) ;
+    int log_level(int new_level) ;
+    int log_level() ;
+    void attach(abstract_log_t *);
+    void detach(abstract_log_t *);
+    void message(int level) ;
+    void message(int level, const char *fmt, ...) ;
+    void message(int level, int line, const char *file, const char *func) ;
+    void message(int level, int line, const char *file, const char *func, const char *fmt, ...) ;
+    void generic(int level, int line, const char *file, const char *func, const char *fmt, va_list arg) ;
+    void get_timestamp() ;
+    void get_struct_tm() ;
+    const char *str_monotonic() ;
+    const char *str_monotonic_nano() ;
+    const char *str_monotonic_micro() ;
+    const char *str_monotonic_milli() ;
+    const char *str_gmt_offset() ;
+    const char *str_tz_abbreviation() ;
+    const char *str_date() ;
+    const char *str_time() ;
+    const char *str_time_micro() ;
+    const char *str_time_milli() ;
+    const char *str_tz_symlink() ;
+    const char *str_name() ;
+    const char *str_pid() ;
+    const char *str_level() ;
+  } ;
 
+  class abstract_log_t
+  {
+    dispatcher_t *dispatcher ;
+    int level, max_level ;
+    int fields ;
+  public:
+    abstract_log_t(int maximal_log_level, dispatcher_t *d) ;
+    int log_level(int new_level) ;
+    int log_level() ;
+    int set_fields(int mask) ;
+    int get_fields() ;
+    int enable_fields(int mask) ;
+    int disable_fields(int mask) ;
+    virtual ~abstract_log_t() ;
+    virtual void compose_message(int level, int line, const char *file, const char *func, const char *fmt, va_list args) ;
+    virtual void submit_message(int level, const char *message) = 0 ;
+  } ;
+
+  class log_file : public abstract_log_t
+  {
+    bool to_be_closed ;
+    log_file(const char *path, int maximal_log_level=qmlog::Debug, dispatcher_t *dispatcher=NULL) ;
+   ~log_file() ;
+    void submit_message(int level, const char *message) ;
+  } ;
+
+  class log_stderr : public abstract_log_t
+  {
+    log_stderr(int maximal_log_level=qmlog::Debug, dispatcher_t *dispatcher=NULL) ;
+   ~log_stderr() ;
+  } ;
+
+  class log_stdout : public abstract_log_t
+  {
+    log_stdout(int maximal_log_level=qmlog::Debug, dispatcher_t *dispatcher=NULL) ;
+   ~log_stdout() ;
+  } ;
+
+  class log_syslog : public abstract_log_t
+  {
+    log_syslog() ;
+   ~log_syslog() ;
+    void submit_message(int level, const char *message) ;
   } ;
 
   class slave_dispatcher_t : public dispatcher_t
   {
   } ;
 
+  class settings_modifier
+  {
+    settings_modifier() ;
+   ~settings_modifier() ;
+  } ;
+
   inline void init() { object.init() ; }
 }
-
-
 
 #endif // QMLOG_DISPATHCER_H
